aa3970853c26953950b039467a69afe06fe1e81a
Tq-Lj <6du@protonmail.com>
2019-08-05 12:46:57 +0800

    .

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 (function(){
-  console.log(1);
+  var path, sodium, config, fs, read;
+  path = require('path');
+  sodium = require('sodium-native');
+  config = require('config-6du/6du');
+  fs = require('fs-extra');
+  read = function(name){
+    return fs.readFile(path.join(__dirname, "../private/key/6du." + name));
+  };
+  (async function(){
+    var message, signed, secret, pk;
+    message = Buffer.from('test');
+    signed = Buffer.alloc(sodium.crypto_sign_BYTES + message.length);
+    secret = (await read('sk'));
+    sodium.crypto_sign(signed, message, secret);
+    console.log(signed);
+    pk = (await read('pk'));
+    message = Buffer.alloc(signed.length - sodium.crypto_sign_BYTES);
+    sodium.crypto_sign_open(message, signed, pk);
+    console.log(message.toString());
+  })();
 }).call(this);
--- index.ls
+++ index.ls
@@ -1,1 +1,33 @@
-console.log 1
+#!/usr/bin/env node -r livescript-transform-implicit-async/register 
+
+require! <[
+  path
+]>
+
+require! {
+  \sodium-native : sodium
+  \config-6du/6du : config
+  \fs-extra : fs
+}
+
+read = (name)~>
+  fs.readFile(
+    path.join(
+      __dirname, "../private/key/6du.#name"
+    )
+  )
+
+do !~>
+  message = Buffer.from \test
+  signed = Buffer.alloc sodium.crypto_sign_BYTES + message.length
+  secret = await read('sk')
+  sodium.crypto_sign(signed, message, secret)
+  console.log signed
+
+  pk = await read('pk')
+  message = Buffer.alloc(
+    signed.length - sodium.crypto_sign_BYTES
+  )
+  sodium.crypto_sign_open(message, signed, pk)
+  console.log message.toString()
+
